d784920664274deb6c2899e9ace558ecaabddb3a
pedro lopes <pepe.ribeiro.lopes@gmail.com>
2026-09-14 09:04:19 -0300

    Update model.py

--- aula08-mini-crm/model.py
+++ aula08-mini-crm/model.py
@@ -1,11 +1,11 @@
 from datetime import date
 
-def model_lead(name, email, company, step = "novo"):
-    """Estrutura um lead como um dicionario """
+def model_lead(name, email, company, step="novo"):
+    """Estrutura um lead como um dicionário"""
     return {
         "name": name,
         "email": email,
         "company": company,
-        "Step": step,
-        "created": date.today().isoformat(),
+        "step": step,
+        "created": date.today().isoformat()
     }
