74635d5ddf08a0fac4ec31cd69a4d1e415158de0
pedro lopes <pepe.ribeiro.lopes@gmail.com>
2026-09-14 09:03:37 -0300

    Update app.py

--- aula08-mini-crm/app.py
+++ aula08-mini-crm/app.py
@@ -7,22 +7,29 @@ def add_lead():
     company = input("Empresa: ")
     step = input("Etapa de vendas: ")
 
-    # validar as entradas do usuario
+    # validar as entradas do usuário
     # depois de validar... vamos modelar os dados
-    print(model_lead(name,email,company,step))
+    print(model_lead(name, email, company, step))
 
     # depois de modelado... vamos enviar esse dict (leads) para o leads.json
-    # para salvar, vamos usar o modulo control
-    control.create_lead(model_lead(name,email,company,step))
+    # para salvar, vamos usar o módulo control
+    control.create_lead(model_lead(name, email, company, step))
 
     print("Lead adicionado (func)")
 
+def list_leads():
+    leads = control.read_leads()
+    if not leads:
+        print("Nenhum lead ainda")
+        return
+
+    print(leads)
 
 def main():
     while True:
         print("\nMini CRM de Leads")
-        print("[1] Adcionar lead")
-        print("[2] Listar lead")
+        print("[1] Adicionar lead")
+        print("[2] Listar leads")
         print("[0] Sair do programa")
 
         opt = input("Escolha uma opção: ")
@@ -30,12 +37,12 @@ def main():
         if opt == "1":
             add_lead()
         elif opt == "2":
-            print("Listar leads")
+            list_leads()
         elif opt == "0":
             print("Até mais...")
             break
         else:
-            print("Opção invalida!")
+            print("Opção inválida")
 
 if __name__ == "__main__":
     main()
