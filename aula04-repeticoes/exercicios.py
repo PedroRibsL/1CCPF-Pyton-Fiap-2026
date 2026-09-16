ea63f855efd2b98ee3a42a82ac192d3cb5e29478
pedro lopes <pepe.ribeiro.lopes@gmail.com>
2026-09-16 11:17:38 -0300

    Update exercicios.py

--- aula04-repeticoes/exercicios.py
+++ aula04-repeticoes/exercicios.py
@@ -80,12 +80,75 @@
 
 # Faça um programa que receba 5 valores digitados pelo usuário e, ao final, informe qual é o maior deles
 
-maior = None
+# maior = None
 
-for i in range(5):
-    n = int(input("Digite o valor: "))
+# for i in range(5):
+#     n = int(input("Digite o valor: "))
     
-    if maior is None or n > maior:
-        maior = n
+#     if maior is None or n > maior:
+#         maior = n
         
-print(f"o maior valor é: {maior}")
+# print(f"o maior valor é: {maior}")
+
+#  Faça um programa capaz de exibir todos os valores pares entre 2 e um valor fornecido pelo usuário.
+
+# valor = int(input("Digite o valor: "))
+
+# for i in range(2, valor + 1, 2):
+#     print(i)
+
+
+#  Escreva um programa que dado um inteiro n positivo calcula e imprime a soma de todos os números
+# inteiros entre 1 e n.
+# ▪ Valide a entrada do usuário, só aceite números positivos!!
+# ▪ Dica: use while para a validação e for para a soma.
+# ▪ Por exemplo, se n = 10 então deverá ser calculado:
+# ▪ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
+# ▪ E a impressão final seria:
+# ▪ A soma de 1 até 10 é: 55
+
+
+# n = int(input("Digite o valor: "))
+
+# while n <= 0:
+#     print("Valor inválido! Digite um número positivo.")
+#     n = int(input("Digite o valor: "))
+    
+# soma = 0
+
+# for i in range(1, n + 1):
+#     soma += i
+
+# print(f"A soma de 1 até {n} é: {soma}") 
+
+
+# Escreva um algoritmo que recebe um inteiro positivo n e imprime todos os divisores positivos de n.
+# ▪ Utilize o laço for.
+# ▪ Exemplo:
+# Suponha que n = 28, nessa situação devemos imprimir os números
+# 1, 2, 4, 7, 14 e 28, que são todos os divisores do 28.
+# ▪ Dica: para o número ser divisor de n, a divisão precisa ter resto nulo.
+
+# n = int(input("Digite o valor: "))
+
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         print(i)
+
+#  Determine e mostre todos os números primos no intervalo de 2 a 2000.
+# Dicas:
+# ▪ Para resolver esse problema, primeiro faça um algoritmo que verifica se um número inteiro qualquer é
+# primo ou não.
+# ▪ A seguir, com esse código em mãos, faça os ajustes necessários para mostrar todos os números primos
+# no intervalo solicitado.
+# ▪ Você precisará colocar uma estrutura de repetição dentro da outra.
+# ▪ Laços aninhados!!!!
+
+for n in range(2, 2001):
+    primo = True
+    
+    for i in range(2, n):
+        if n % i == 0:
+            primo = False
+    if primo == True:
+        print(n)
